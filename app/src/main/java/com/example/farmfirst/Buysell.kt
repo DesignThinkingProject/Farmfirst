a6a43e51c5ff53e4508cd71377814b231b781dce
A191254 <atharvpw.is19@rvce.edu.in>
2021-06-08 21:05:49 +0530

    Added buttons....now we can start the project

---
+++ app/src/main/java/com/example/farmfirst/Buysell.kt
@@ -0,0 +1,12 @@
+package com.example.farmfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buysell : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buysell)
+
+    }
+}
--- app/src/main/java/com/example/farmfirst/MainActivity.kt
+++ app/src/main/java/com/example/farmfirst/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.farmfirst
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            val i= Intent(this,Buysell::class.java)
+        }
+
+
+        btn2.setOnClickListener {
+            val i= Intent(this,Buysell::class.java)
+        }
+
+        btn3.setOnClickListener {
+            val i= Intent(this,Buysell::class.java)
+        }
     }
 }
