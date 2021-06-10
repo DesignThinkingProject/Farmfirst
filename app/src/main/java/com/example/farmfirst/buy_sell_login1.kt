5862536aa227e4026cdd01ef584c03dd48180339
charansaigorla <gorlacharansai2002@gmail.com>
2021-06-10 21:40:34 +0530

    added buysell login UI

---
+++ app/src/main/java/com/example/farmfirst/buy_sell_login1.kt
@@ -0,0 +1,11 @@
+package com.example.farmfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buy_sell_login1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_sell_login1)
+    }
+}
