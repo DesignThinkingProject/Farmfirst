0d675d62df6589c90e369e6a46c4b6fcef4f32b7
Shivam <shivamp.is19@rvce.edu.in>
2021-07-25 00:20:38 +0530

    User details

--- app/src/main/java/com/example/farmfirst/MainActivity.kt
+++ app/src/main/java/com/example/farmfirst/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dbRef = FirebaseDatabase.getInstance().getReference("/users/")
-
-
-
+        readData();
 
         btn1.setOnClickListener {
             val i= Intent(this, Buysell::class.java)
@@ -38,5 +37,30 @@ class MainActivity : AppCompatActivity() {
             val i= Intent(this,Govtschemes::class.java)
             startActivity(i)
         }
+    }
+    private fun readData() {
+        dbRef = FirebaseDatabase.getInstance().getReference("users")
+        if (uid != null) {
+            dbRef.child(uid).get().addOnSuccessListener {
+
+                if(it.exists()){
+                    val location = it.child("location").value
+                    val mobileno = it.child("mobileno").value
+                    var username = it.child("username").value
+                    username = " Hi, $username \n Mobile no: $mobileno \n Location: $location"
+                    detailsid.text = username.toString()
+
+                }
+                else{
+                    Toast.makeText(this, "User Doesn't Exist ! ",Toast.LENGTH_SHORT).show()
+                }
+
+
+            }.addOnFailureListener{
+                Toast.makeText(this, "Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
