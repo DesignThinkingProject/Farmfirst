166475e117754414d5714f7562ada0498de9b6c6
Nachiketa Nalin <nalinnachiketa@gmail.com>
2021-08-13 23:55:39 +0530

    labours in firebase now

--- app/src/main/java/com/example/farmfirst/LabourRequest.kt
+++ app/src/main/java/com/example/farmfirst/LabourRequest.kt
@@ -1,11 +1,53 @@
 package com.example.farmfirst
 
+import com.example.farmfirst.databinding.ActivityLabourRequestBinding
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.farmfirst.databinding.ActivityRequestBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+
+private const val TAGLOG = "SignTag"
+
+
 
 class LabourRequest : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLabourRequestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_labour_request)
+        binding = ActivityLabourRequestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveBtn1.setOnClickListener {
+            saveLabourToFirebaseDatabase()
+            val i= Intent(this, Labours::class.java)
+            startActivity(i)
+        }
+
     }
+
+
+    private fun saveLabourToFirebaseDatabase() {
+        val uid = FirebaseAuth.getInstance().uid
+        val ref1 = FirebaseDatabase.getInstance().getReference("/labourRequest/")
+
+        val labourdetail = Labourdetails(
+            binding.NameEt1.text.toString(),
+            binding.LocationEt1.text.toString(),
+            binding.daysEt1.text.toString().toInt(),
+            binding.noteEt1.text.toString().toInt(),
+            binding.dateEt1.text.toString(),
+            binding.wageEt2.text.toString()
+        )
+        //Log.d("TAGLOG", "saveCropToFirebaseDatabase: $name $location $weight $amount $note $date")
+        ref1.child("$uid").setValue(labourdetail)
+    }
+
 }
+data class Labourdetails(val name1: String?=null, val location: String?=null, val days: Int?=null,
+                         val wage: Int?=null, val phone: String?=null, val date : String?=null)
--- app/src/main/java/com/example/farmfirst/Labours.kt
+++ app/src/main/java/com/example/farmfirst/Labours.kt
@@ -1,11 +1,20 @@
 package com.example.farmfirst
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Labours : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_labours)
+    }
+
+    fun openNewTask(view: View) {
+
+        startActivity(Intent(this, LabourRequest :: class.java))
+
     }
 }
