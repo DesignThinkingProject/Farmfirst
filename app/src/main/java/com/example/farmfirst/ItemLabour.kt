8714526484a03fbcea4853c2882e3c2087f0e40b
charansaigorla <gorlacharansai2002@gmail.com>
2021-08-13 17:33:58 +0530

    labour page updated

---
+++ app/src/main/java/com/example/farmfirst/ItemLabour.kt
@@ -0,0 +1,11 @@
+package com.example.farmfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemLabour : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_labour)
+    }
+}
---
+++ app/src/main/java/com/example/farmfirst/LabourRequest.kt
@@ -0,0 +1,11 @@
+package com.example.farmfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LabourRequest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_labour_request)
+    }
+}
