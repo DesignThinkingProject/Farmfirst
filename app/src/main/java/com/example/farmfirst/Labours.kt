608e2220fc700ac50034825d7e5b385f5e9c30c0
charansaigorla <gorlacharansai2002@gmail.com>
2021-08-17 00:11:49 +0530

    Buysell UI changed

--- app/src/main/java/com/example/farmfirst/Labours.kt
+++ app/src/main/java/com/example/farmfirst/Labours.kt
@@ -4,12 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class Labours : AppCompatActivity() {
+    private lateinit var dbRef : DatabaseReference
+    val uid = FirebaseAuth.getInstance().uid
+    private lateinit var myrecyclerview : RecyclerView
+    private lateinit var mylist : ArrayList<Labourdetails>
+    private lateinit var labourdetail : Labourdetails
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_labours)
+        myrecyclerview = findViewById(R.id.listofrequests)
+        myrecyclerview.layoutManager = LinearLayoutManager(this)
+
+        mylist = arrayListOf<Labourdetails>()
+        getLabourdata()
+    }
+    private fun getLabourdata() {
+
+        dbRef = FirebaseDatabase.getInstance().getReference("/labourRequest/")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (userSnapshot in snapshot.children) {
+                        labourdetail = userSnapshot.getValue(Labourdetails::class.java)!!
+                        mylist.add(labourdetail)
+                    }
+                    myrecyclerview.adapter = MyAdapterBuysell(mylist)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
     }
 
     fun openNewTask(view: View) {
---
+++ app/src/main/java/com/example/farmfirst/MyLabourAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.farmfirst
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyLabourAdapter(private val detailslist :ArrayList<Labourdetails>) : RecyclerView.Adapter<MyLabourAdapter.MyViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_labour
+            ,parent,false)
+        return MyViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentdetail =  detailslist[position]
+
+        holder.showTitle.text = currentdetail.name1
+        holder.showLocation.text = currentdetail.location
+        holder.showMoney.text = currentdetail.amount.toString()
+        holder.showQuantity.text = currentdetail.weight1.toString()
+        holder.showDate.text = currentdetail.date
+    }
+
+    override fun getItemCount(): Int {
+        return detailslist.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val showTitle : TextView = itemView.findViewById(R.id.txtShowTitle)
+        val showQuantity : TextView = itemView.findViewById(R.id.txtShowQuantity)
+        val showMoney : TextView = itemView.findViewById(R.id.txtMoney)
+        val showName : TextView = itemView.findViewById(R.id.txtShowName)
+        val showLocation : TextView = itemView.findViewById(R.id.txtLocation)
+        val showDate : TextView = itemView.findViewById(R.id.txtShowDate)
+    }
+
+//    override fun onBindViewHolder(holder: MyAdapterBuysell.MyViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+}
